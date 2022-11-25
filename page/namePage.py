3381d46108a9613a99cc3bf2f979527c5e0ad769
yoonjujeon <105338895+yoonjujeon@users.noreply.github.com>
2022-11-26 01:41:40 +0900

    이름학번페이지 완성

--- page/namePage.py
+++ page/namePage.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QStackedWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QLineEdit, QRadioButton, QButtonGroup, QProgressBar, QGraphicsView, QGraphicsScene, QGraphicsProxyWidget
 from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QFontDatabase, QPixmap
 from qt_material import apply_stylesheet
 
 from define.questions import Question
@@ -16,8 +17,13 @@ class NamePage(QWidget):
     # UI 설정
     def initUI(self):
 
+        self.fdb = QFontDatabase()
+        self.fdb.addApplicationFont('./SeoulNamsanB.ttf')
+        self.fdb.addApplicationFont('./SeoulNamsanEB.ttf')
         
         hTopBox = QHBoxLayout()
+        hContentBox = QHBoxLayout()
+        hMiddleLeftBox = QHBoxLayout()
         hMiddle1Box = QHBoxLayout()
         hMiddle2Box = QHBoxLayout()
 
@@ -25,10 +31,24 @@ class NamePage(QWidget):
         self.nameEdit = QLineEdit()
 
         uiGroups = {
-            'title' : {'layout': hTopBox, 'widget': [QLabel('MBTI 검사지(미정)')]},
+            'title' : {'layout': hTopBox, 'widget': [QLabel('소융대 MBTI 검사')]},
+            'content': {'layout': hContentBox, 'widget': [QLabel('<소융대 MBTI 검사>는\n자신의 MBTI와 잘 맞는 학부생을 찾을 수 있는\n소프트웨어 융합대학 학부생 전용 MBTI 검사입니다.')]},
+            'koomin': {'layout': hMiddleLeftBox, 'widget': [QLabel()]},
             'number' : {'layout': hMiddle1Box, 'widget': [QLabel('학번: '), self.numberEdit]},
             'name': {'layout': hMiddle2Box, 'widget': [QLabel('이름: '), self.nameEdit]}
         }
+
+        uiGroups['content']['widget'][0].setAlignment(Qt.AlignCenter)
+        pixmap = QPixmap('쿠민.jpg')
+        pixmap = pixmap.scaledToWidth(self.width() // 2)
+        uiGroups['koomin']['widget'][0].setPixmap(pixmap)
+        uiGroups['koomin']['widget'][0].setStyleSheet('border: 1px solid lightgray;')
+
+        # 폰트 변경
+        uiGroups['title']['widget'][0].setStyleSheet('font-family: 서울남산체 EB; font-size: 40pt;')
+        uiGroups['content']['widget'][0].setStyleSheet('font-family: 서울남산체 B; font-size: 15pt;')
+        uiGroups['number']['widget'][0].setStyleSheet('font-family: 서울남산체 B; font-size: 15pt;')
+        uiGroups['name']['widget'][0].setStyleSheet('font-family: 서울남산체 B; font-size: 15pt;')
 
         for label in uiGroups.keys():
             layout = uiGroups[label]['layout']
@@ -37,9 +57,23 @@ class NamePage(QWidget):
                 layout.addWidget(widget)
             layout.addStretch(1)
 
+        hMiddleRightBox = QVBoxLayout()
+        hMiddleRightBox.addStretch(1)
+        hMiddleRightBox.addLayout(hMiddle1Box)
+        hMiddleRightBox.addLayout(hMiddle2Box)
+        hMiddleBox = QHBoxLayout()
+        hMiddleBox.addStretch(1)
+        hMiddleBox.addLayout(hMiddleLeftBox)
+        hMiddleBox.addSpacing(20)
+        hMiddleBox.addLayout(hMiddleRightBox)
+        hMiddleBox.addStretch(1)
+
         # 다음 페이지 버튼 UI
         hBttomBox = QHBoxLayout()
         nextPageBtn = QPushButton("시작하기")
+        nextPageBtn.setStyleSheet("font-family: 서울남산체 EB; font-size: 40pt;")
+        nextPageBtn.setMinimumWidth(self.width() // 2)
+        nextPageBtn.setMinimumHeight(nextPageBtn.height() // 3)
         # nextPageBtn.setStyleSheet("color: blue;"
         #                       "background-color: #87CEFA;"
         #                       "border-style: dashed;"
@@ -48,15 +82,19 @@ class NamePage(QWidget):
         nextPageBtn.clicked.connect(self.questionPage)
         hBttomBox.addStretch(1)
         hBttomBox.addWidget(nextPageBtn)
+        hBttomBox.addStretch(1)
 
         # UI 위치 조정
         vBox= QVBoxLayout()
+        vBox.addStretch(1)
         vBox.addLayout(hTopBox)
         vBox.addStretch(1)
-        vBox.addLayout(hMiddle1Box)
-        vBox.addLayout(hMiddle2Box)
+        vBox.addLayout(hContentBox)
+        vBox.addStretch(1)
+        vBox.addLayout(hMiddleBox)
         vBox.addStretch(1)
         vBox.addLayout(hBttomBox)
+        vBox.addStretch(1)
         
         self.setLayout(vBox)
 
