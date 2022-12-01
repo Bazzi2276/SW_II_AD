1fda5769524490e240d257c24653db16ab23ca3f
Bazzi4263 <113406762+Bazzi2276@users.noreply.github.com>
2022-12-01 15:33:42 +0900

    Values 단위 테스트 추가

--- define/testValues.py
+++ define/testValues.py
@@ -1,8 +1,26 @@
-import unittest
+﻿import unittest
 from values import Values
 
 class TestValues(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.v1 = Values()
+
+    def tearDown(self):
+        pass
+
+    def testFindSuitedMBTI(self):
+        self.assertEqual(self.v1.findSuitedMBTI("ISTJ"), ["ESFP", "ESTP"])
+
+    def testFindSuitedCelebrity(self):
+        self.assertEqual(self.v1.findSuitedCelebrity("ISTJ"), {"전소민":"ESFP", "정일훈":"ESTP"})
+
+    def testFindSuitedStudent(self):
+        self.assertEqual(self.v1.findSuitedStudent("ISTJ"), {"전현육":"ESTP"})
+
+    def testFindStudentNumber(self):
+        self.assertEqual(self.v1.findStudentNumber("전현빈"), "20223134")
+
 
 if __name__ == '__main__':
     unittest.main()
